acd5366dd0d53e6a1c6010df14d084dcf69ca111
Byzernn <62064504+Byzernn@users.noreply.github.com>
2021-11-11 00:10:53 +0800

    Update Register.php

--- Register (user)/Register.php
+++ Register (user)/Register.php
@@ -1,25 +1,31 @@
 <?php
-  include 'koneksi.php';
+  include 'koneksi.php'; 
 
-    if(isset($_POST['signup'])){
-  
-  	  	//proses insert
-          $insert = mysqli_query($connect, "INSERT INTO user VALUES (
-                    	'".$generateId."',
-			'".date('Y-m-d')."',
-			'".$_POST['name']."',
-			'".$_POST['email']."'
-                    	'".$_POST['pass']."'
-	)");
+    if (isset($_POST['signup'])){
 
-		if($insert){
-			echo '<script>window.location="Register.php"</script>';
-		}else{
-			echo "huft" .mysqli_error($connect);
-		}
-
-	}
-
+      $getMaxId = mysqli_query($connect, "SELECT MAX(RIGHT(id_user, 5)) AS id FROM register");
+      $d = mysqli_fetch_object($getMaxId);
+      $generateId = 'P'.date('Y').sprintf("%05s", $d->id + 1);
+      
+      //proses insert
+		  $insert = mysqli_query($connect, "INSERT INTO register VALUES (
+              '".$generateId."',
+              '".date('Y-m-d')."',
+              '".$_POST['name']."',
+              '".$_POST['email']."',
+              '".$_POST['pass']."'
+              )");
+        
+              if($insert){
+                echo "<script>
+                      alert('Register Berhasil !');
+                      document.location.href = 'Register.php';
+                      </script>";
+              }else{
+                echo  'Register Gagal !'
+                      .mysqli_error($connect);
+              }
+    }
 ?>
 
 <!DOCTYPE html>
