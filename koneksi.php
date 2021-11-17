6e8e79d0a10bd1ab1a8ead5c859bc8aee9ab59e1
Ahmad Rusyadi <gerdasofya@outlook.co.id>
2021-11-18 07:36:14 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,13 @@
 <?php
-$dbhost = 'localhost'; 
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'dcampdb';
+//$dbhost = 'localhost'; 
+//$dbuser = 'root';
+//$dbpass = '';
+//$dbname = 'dcampdb';
 
-// $dbhost = 'sql105.epizy.com'; 
-// $dbuser = 'epiz_30331140';
-// $dbpass = 'undabungas';
-// $dbname = 'epiz_30331140_dcampdb';
+ $dbhost = 'sql105.epizy.com'; 
+ $dbuser = 'epiz_30331140';
+ $dbpass = 'undabungas';
+ $dbname = 'epiz_30331140_dcampdb';
 
 $connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die('koneksi gagal');
 ?>
